a3b8571bca9e93393f1e3225748713191b8530c0
Ayrat Badykov <ayratin555@gmail.com>
2015-02-23 20:13:26 +0300

    Добавлен ActionBar с изображением и текстом для видов спорта

--- app/src/main/java/com/badykov/ayrat/swkazan/SportActivity.java
+++ app/src/main/java/com/badykov/ayrat/swkazan/SportActivity.java
@@ -1,10 +1,14 @@
 package com.badykov.ayrat.swkazan;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -24,6 +28,38 @@ public class SportActivity extends ActionBarActivity {
         setDistrictName();
         populateListviewWithDistricts();
         setOnListViewItemClickAction();
+        customizeActionBar();
+
+    }
+
+    private void customizeActionBar(){
+        LayoutInflater mInflater = LayoutInflater.from(this);
+        View actionbarView = mInflater.inflate(R.layout.action_bar, null);
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayOptions(
+                ActionBar.DISPLAY_SHOW_CUSTOM, ActionBar.DISPLAY_SHOW_CUSTOM |
+                        ActionBar.DISPLAY_SHOW_HOME |
+                        ActionBar.DISPLAY_SHOW_TITLE);
+
+
+
+
+        TextView actionbarTextView=(TextView)actionbarView.findViewById(R.id.text);
+        ImageView actionbarImageView=(ImageView)actionbarView.findViewById(R.id.image);
+        actionbarTextView.setText(R.string.sport_select);
+
+        actionbarImageView.setImageResource(R.drawable.sport);
+        actionBar.setCustomView(actionbarView,
+                new ActionBar.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                )
+        );
+
+
+
     }
 
     private void populateListviewWithDistricts(){
