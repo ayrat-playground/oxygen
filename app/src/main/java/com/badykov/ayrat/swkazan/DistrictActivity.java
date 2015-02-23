4930aa69a7b4460ff340a21027025e643ebc60ec
Ayrat Badykov <ayratin555@gmail.com>
2015-02-23 19:13:24 +0300

    В первый активити добавлен ActionBar

--- app/src/main/java/com/badykov/ayrat/swkazan/DistrictActivity.java
+++ app/src/main/java/com/badykov/ayrat/swkazan/DistrictActivity.java
@@ -1,11 +1,16 @@
 package com.badykov.ayrat.swkazan;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,6 +32,36 @@ public class DistrictActivity extends ActionBarActivity {
 
         populateListviewWithDistricts();
         setOnListViewItemClickAction();
+        customizeActionBar();
+
+    }
+    private void customizeActionBar(){
+        LayoutInflater mInflater = LayoutInflater.from(this);
+        View actionbarView = mInflater.inflate(R.layout.action_bar, null);
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayOptions(
+                ActionBar.DISPLAY_SHOW_CUSTOM, ActionBar.DISPLAY_SHOW_CUSTOM |
+                        ActionBar.DISPLAY_SHOW_HOME |
+                        ActionBar.DISPLAY_SHOW_TITLE);
+
+
+
+
+        TextView actionbarTextView=(TextView)actionbarView.findViewById(R.id.text);
+        ImageView actionbarImageView=(ImageView)actionbarView.findViewById(R.id.image);
+        actionbarTextView.setText(R.string.district_select);
+
+        actionbarImageView.setImageResource(R.drawable.district);
+        actionBar.setCustomView(actionbarView,
+                new ActionBar.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                )
+        );
+
+
 
     }
 
